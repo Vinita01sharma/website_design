3c63fad3c2120671f672dbb2aea83cb6aa506521
Vinita01sharma <136809090+Vinita01sharma@users.noreply.github.com>
2024-02-22 12:05:36 +0530

    new

---
+++ Ad.js/04/03_operator.js
@@ -0,0 +1,66 @@
+/*
+// "" -- null -- undefined - 0 - false => falsy value
+let m = "10";
+let m2 = 10;
+
+console.log("----------- m == m2----------->", m == m2);
+console.log("----------- m === m2 ----------->", m === m2);
+*/
+
+/*
+if (10 == 10 || 20 == 10) {
+  console.log("---1-->");
+}
+if (true || true) {
+  console.log("---2-->");
+}
+
+if (true && false) {
+  console.log("---3-->");
+}
+if (true && true) {
+  console.log("---4-->");
+}
+
+
+// aplly conditions and chek what answer is coming in above given conditions
+
+// if(10==10 || 20==10){
+//     console.log("-----1---->");
+// }
+
+// if(10==10 || 10==10){
+//     console.log("-----2---->");
+// }
+
+// if(10==10 && 20==10){
+//     console.log("-----3------>");
+// }
+
+
+// if(10==10 && 10==10){
+//     console.log("-----4---->");
+// }
+
+
+
+ t || f = t
+ f || f = f
+ f && t = f
+ t && t = t
+*/
+
+let x = "urvish"; // "" / null / undefined / 0 /false
+if (x && x !== "test") {
+  console.log("-----------  x----------->", x);
+} else {
+  console.log("nothing have x");
+}
+
+
+// let x = "urvish"; // "urvish,""
+// if(x || ){
+//     console.log("----x-------", x);
+// }else{
+//     console.log("nothing have x");
+// }
