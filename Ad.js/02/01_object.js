049dd48d9667d2da4c6e40c81142ba46e5352c17
Vinita01sharma <136809090+Vinita01sharma@users.noreply.github.com>
2024-02-27 13:10:30 +0530

    new

--- Ad.js/02/01_object.js
+++ Ad.js/02/01_object.js
@@ -29,20 +29,4 @@ let person = {
   
   obj.print();
   
-  let productData = {
-    title: "Flying Wooden Bird",
-    description:
-      "Package Include 6 Birds with Adhesive Tape Shape: 3D Shaped Wooden Birds Material: Wooden MDF, Laminated 3.5mm",
-    brand: "Oppo",
-    gender: "male",
-    price: 51,
-    discountPercentage: 15.58,
-    category: ["smartphones", "3g", "4g"],
-    color: ["red", "black", "white"],
-    size: ["44", "45", "42", "43"],
-    isAvailable: true,
-    availableStock: 17,
-    rating: 0,
-    totalRaters: 0,
-    totalSoldUnit: 0,
-  };
+  
