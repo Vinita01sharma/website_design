ee55275de8376e6a441fe0f4b5144fa85493f7bc
Vinita01sharma. <vinita0122sharma@gmail.com.>
2024-08-13 09:57:34 +0530

    new

---
+++ react/UrbanTibe/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./ui/Component/Header/Header.jsx";
+import Footer from "./ui/Component/Footer/Foter.jsx";
+
+
+function App() {
+
+  return (<>
+    { /* ------------HEADER------------ */ }
+    <Header/> 
+
+
+    {/* ------------FOOTER------------ */ }
+    <Footer/> 
+  </>
+    
+  )
+}
+
+export default App
